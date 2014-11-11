0406290f7bbb0f3d292301c8677af872b6ebc68c
Benjamin Eberlei <kontakt@beberlei.de>
2014-11-12 00:46:05 +0100

    Initial

---
+++ src/Acme/HelloBundle/AcmeHelloBundle.php
@@ -0,0 +1,10 @@
+<?php
+// src/Acme/HelloBundle/AcmeHelloBundle.php
+
+namespace Acme\HelloBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeHelloBundle extends Bundle
+{
+}
---
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Acme/HelloBundle/Controller/DefaultController.php
+
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function helloAction($name)
+    {
+        return $this->render(
+            'AcmeHelloBundle:Default:hello.html.twig',
+            array('name' => $name)
+        );
+    }
+}
